3e1d15d221c0275d25d355ffa3a75046e71a8fbd
hisokaBK <bilalbakessou99@gmail.com>
2026-04-04 18:42:20 +0100

    feat(auth): install Laravel Sanctum for API authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test',function(){
+       return response()->json([
+          'message'=>'ok'
+       ]);
+})->middleware('auth:sanctum');
+
+
+
+Route::middleware('auth:sanctum')->get('/user-test', function (Request $request) {
+    return response()->json([
+        'user' => $request->user()
+    ]);
+});
