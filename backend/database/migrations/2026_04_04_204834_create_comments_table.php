89b48179f6f4ec6779755781bad5aca18f05c806
hisokaBK <bilalbakessou99@gmail.com>
2026-04-04 22:06:51 +0100

    feat(comments): create comments table linked to users and videos

--- backend/database/migrations/2026_04_04_204834_create_comments_table.php
+++ backend/database/migrations/2026_04_04_204834_create_comments_table.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('video_id')->constrained()->cascadeOnDelete();
+            $table->text('content');
+            $table->timestamps();
+        });
     }
 
     /**
