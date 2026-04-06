e9afab0191a7a37d60f09bdc169e5b8febb58391
hisokaBK <bilalbakessou99@gmail.com>
2026-04-06 18:56:16 +0100

    feat(auth):create AuthController and implement register with Sanctum

--- backend/app/Http/Controllers/Api/Auth/AuthController.php
+++ backend/app/Http/Controllers/Api/Auth/AuthController.php
@@ -1,11 +1,48 @@
 <?php
 
-namespace App\Http\Controllers\Api\Auth;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Models\User;
+use App\Models\Profile;
+use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $user = User::create([
+            'name' => $request->validated('name'),
+            'email' => $request->validated('email'),
+            'password' => $request->validated('password'),
+            'role' => 'user',
+        ]);
+
+        Profile::create([
+            'user_id' => $user->id,
+            'avatar' => null,
+            'background_image' => null,
+            'bio' => null,
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $user->load('profile');
+
+        return response()->json([
+            'message' => 'Register successful',
+            'data' => [
+                'user' => new UserResource($user),
+                'token' => $token,
+            ],
+        ], 201);
+    }
+
+
+
+
 }
