1469ae12b8af5cebd8d329b9aa6dad77c2fb1720
hisokaBK <bilalbakessou99@gmail.com>
2026-04-20 19:20:47 +0100

    feat(livekit): add token generation service

---
+++ backend/app/Services/LiveKitService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Stream;
+use App\Models\User;
+use Agence104\LiveKit\AccessToken;
+use Agence104\LiveKit\AccessTokenOptions;
+use Agence104\LiveKit\VideoGrant;
+
+class LiveKitService
+{
+    public function createBroadcasterToken(Stream $stream, User $user): string
+    {
+        $tokenOptions = (new AccessTokenOptions())
+            ->setIdentity('broadcaster-' . $user->id)
+            ->setName($user->name);
+
+        $videoGrant = (new VideoGrant())
+            ->setRoomJoin()
+            ->setRoomName($stream->room_name)
+            ->setCanPublish(true)
+            ->setCanSubscribe(true)
+            ->setCanPublishData(true);
+
+        return (new AccessToken(
+            env('LIVEKIT_API_KEY'),
+            env('LIVEKIT_API_SECRET')
+        ))
+            ->init($tokenOptions)
+            ->setGrant($videoGrant)
+            ->toJwt();
+    }
+
+    public function createViewerToken(Stream $stream, User $user): string
+    {
+        $tokenOptions = (new AccessTokenOptions())
+            ->setIdentity('viewer-' . $user->id . '-' . uniqid())
+            ->setName($user->name);
+
+        $videoGrant = (new VideoGrant())
+            ->setRoomJoin()
+            ->setRoomName($stream->room_name)
+            ->setCanPublish(false)
+            ->setCanSubscribe(true)
+            ->setCanPublishData(false);
+
+        return (new AccessToken(
+            env('LIVEKIT_API_KEY'),
+            env('LIVEKIT_API_SECRET')
+        ))
+            ->init($tokenOptions)
+            ->setGrant($videoGrant)
+            ->toJwt();
+    }
+}
